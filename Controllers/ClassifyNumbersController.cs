e69ad98784a3bbb2a08889417ea12eb556a00a4a
amaechijude <ugwuamaechi5@gmail.com>
2025-02-03 08:12:23 +0100

    Add project files.

---
+++ Controllers/ClassifyNumbersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hng12Stage1.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ClassifyNumbersController : ControllerBase
+    {
+        [HttpGet("classify-number/{number}")]
+        public IActionResult ClassifyNumber([FromQuery] string number)
+        {
+            bool isDigit = int.TryParse(number, out int num);
+            if (!isDigit)
+                return BadRequest( new
+                {
+                    number = "alphabet",
+                    error = "true"
+                });
+
+            return Ok( new
+            {
+                number,
+                is_prime = IsPrime(num),
+                is_perfect = IsPerfect(num),
+                properties = GetProperties(num),
+                digit_sum = GetDigitSum(num),
+                fun_fact = GetFunFact(num)
+            });
+        }
+
+}
+}
