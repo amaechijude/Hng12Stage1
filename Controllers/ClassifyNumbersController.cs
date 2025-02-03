f196a8b68e394f701dc77b3555772649a704527a
amaechijude <ugwuamaechi5@gmail.com>
2025-02-03 10:09:20 +0100

    ready to deploy

--- Controllers/ClassifyNumbersController.cs
+++ Controllers/ClassifyNumbersController.cs
@@ -7,27 +7,92 @@ namespace Hng12Stage1.Controllers
     [ApiController]
     public class ClassifyNumbersController : ControllerBase
     {
-        [HttpGet("classify-number/{number}")]
-        public IActionResult ClassifyNumber([FromQuery] string number)
+        [HttpGet("classify-number")]
+        public async Task<IActionResult> ClassifyNumber([FromQuery] string number="371")
         {
             bool isDigit = int.TryParse(number, out int num);
             if (!isDigit)
                 return BadRequest( new
                 {
                     number = "alphabet",
-                    error = "true"
+                    error = true
                 });
 
             return Ok( new
             {
-                number,
+                number = num,
                 is_prime = IsPrime(num),
                 is_perfect = IsPerfect(num),
                 properties = GetProperties(num),
                 digit_sum = GetDigitSum(num),
-                fun_fact = GetFunFact(num)
+                fun_fact = await GetFunFactAsync(number)
             });
         }
 
-}
+        private static bool IsPrime(int num)
+        {
+            if (num < 2)
+                return false;
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private static bool IsPerfect(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                    sum += i;
+            }
+            return sum == num;
+        }
+
+        private static string[] GetProperties(int num)
+        {
+            if (IsArmstrong(num))
+                return ["armstrong", IsEven(num)];
+
+            return [IsEven(num)];
+        }
+
+        private static bool IsArmstrong(int num)
+        {
+            int sum = 0;
+            int temp = num;
+            int length = num.ToString().Length;
+            while (temp != 0)
+            {
+                int remainder = temp % 10;
+                sum += (int)Math.Pow(remainder, length);
+                temp /= 10;
+            }
+            return sum == num;
+        }
+        private static string IsEven(int num)
+        {
+            return num % 2 == 0 ? "even" : "odd";
+        }
+        private static int GetDigitSum(int num)
+        {
+            int sum = 0;
+            while (num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+
+        private static async Task<string> GetFunFactAsync(string num)
+        {
+            HttpClient client = new();
+            HttpResponseMessage response = await client.GetAsync($"http://numbersapi.com/{num}/math");
+            return $"{await response.Content.ReadAsStringAsync()}";
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# Hng12Stage1
+# Number Information API
+
+## Overview
+
+The **Number Information API** provides details about a given number, including its primality, perfection status, properties, digit sum, and a fun fact.
+The properties of the number include whether it is an _Armstrong Number_ and also checks if the number is _even_ or _odd_.
+The funfact is retrieved from the [Numbers API]("http://numbersapi.com/371/math).
+
+## Endpoint
+
+`GET /api/classify-number/`
+
+## Query Parameters
+
+| Parameter | Type   | Required | Description |
+|-----------|--------|----------|-------------|
+| number    | int    | Yes      | The number to retrieve information about. |
+
+## Response Format
+
+The API returns a JSON response with the following structure:
+
+```json
+{
+    "number": 371,
+    "is_prime": false,
+    "is_perfect": false,
+    "properties": [
+      "armstrong",
+      "odd"
+    ],
+    "digit_sum": 11,
+    "fun_fact": "371 is a narcissistic number."
+}
+```
+
+## Example Request
+
+```http
+GET /api/classify-number/?number=371
+```
+
+## Example Response
+
+```json
+{
+    "number": 371,
+    "is_prime": false,
+    "is_perfect": false,
+    "properties": [
+      "armstrong",
+      "odd"
+    ],
+    "digit_sum": 11,
+    "fun_fact": "371 is a narcissistic number."
+}
+```
+
+## Error Handling
+
+If an invalid number parameter is provided, the API returns an appropriate error response:
+
+```json
+{
+    "number": "alphabet",
+    "error": true
+}
+```
